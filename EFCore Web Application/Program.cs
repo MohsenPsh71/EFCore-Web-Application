7c509b511a88e6f3cc4c9c7665af46c64203c853
mohsen <71mohsenpsh@gmail.com>
2024-09-05 15:21:09 +0330

    add dbcontext And Categories Table

--- EFCore Web Application/Program.cs
+++ EFCore Web Application/Program.cs
@@ -1,7 +1,17 @@
+using EFCoreWebApplication_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
