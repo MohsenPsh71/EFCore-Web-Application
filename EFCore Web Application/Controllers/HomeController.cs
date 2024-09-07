aba32ed54d5102cf0d06b150cec90d4fae2fe7a3
mohsen <71mohsenpsh@gmail.com>
2024-09-08 02:40:45 +0330

    1.add Actions Upsert, AddMultipleRecords, AddMultipleRecords2 and RemoveRange on Home Controller 2.add Index and Upsert Views

--- EFCore Web Application/Controllers/HomeController.cs
+++ EFCore Web Application/Controllers/HomeController.cs
@@ -1,21 +1,93 @@
+using EFCoreWebApplication_DataAccess.Data;
 using EFCoreWebApplication_Model.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EFCore_Web_Application.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Category> list = _context.Categories.ToList();
+            return View(list);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            if (id == null)
+                return View(category);
+
+            category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Category category)
+        {
+            if (category.Id == 0)
+                _context.Categories.Add(category);
+            else
+            {
+                _context.Categories.Update(category);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.First(c => c.Id == id);
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult AddMultipleRecords()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _context.Categories.Add(new Category()
+                {
+                    Name = "Category " + i
+                });
+
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult AddMultipleRecords2()
+        {
+            List<Category> list = new List<Category>();
+            for (int i = 20; i < 30; i++)
+            {
+                list.Add(new Category() { Name = $"Cat {i}" });
+            }
+            _context.Categories.AddRange(list);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveRange()
+        {
+            List<Category> list = _context.Categories.OrderByDescending(c => c.Id).Take(5).ToList();
+            _context.Categories.RemoveRange(list);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
